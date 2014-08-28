429e99046ed27b4856db29d2f46dcad32feb1690
Uli Preuss <me@ulipreuss.eu>
2014-08-29 01:03:18 +0200

    some cleanup

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
+// Create a node-static server instance 
+// to serve the './public' folder
+
 var static = require('node-static');
 var http = require('http');
-//
-// Create a node-static server instance to serve the './public' folder
-//
+
 var file = new static.Server('./public');
 
 http.createServer(function (request, response) {
-    request.addListener('end', function () {
-        file.serve(request, response);
-    }).resume();
+  request.addListener('end', function () {
+    file.serve(request, response);
+  }).resume();
 }).listen(3000);
 
 console.log("Server is listening on localhost:3000");
